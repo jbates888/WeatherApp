51f10a75fdfeaeb72fb2f22d541ff0cf75db7e70
jbates888 <jackbates555@gmail.com>
2020-11-24 10:37:57 -0600

    Updated widget click and text

--- app/src/main/java/com/example/AppWidget/WeatherWidget.kt
+++ app/src/main/java/com/example/AppWidget/WeatherWidget.kt
@@ -1,13 +1,15 @@
 package com.example.AppWidget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
-import androidx.lifecycle.ViewModelProvider
 import com.example.forecast.R
+import com.example.forecast.ui.MainActivity
 import com.example.forecast.ui.weather.current.CurrentWeatherViewModel
-import com.example.forecast.ui.weather.current.CurrentWeatherViewModelFactory
+
 
 /**
  * Implementation of App Widget functionality.
@@ -15,11 +17,7 @@ import com.example.forecast.ui.weather.current.CurrentWeatherViewModelFactory
 class WeatherWidget : AppWidgetProvider() {
 
     private lateinit var viewModel: CurrentWeatherViewModel
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -35,15 +33,16 @@ class WeatherWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val widgetText = "text for widget"
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.weather_widget)
 
+    views.setTextViewText(R.id.widget_temp, widgetText);
+    /** PendingIntent to launch the MainActivity when the widget was clicked  */
+    val launchMain = Intent(context, MainActivity::class.java)
+    val pendingMainIntent = PendingIntent.getActivity(context, 0, launchMain, 0)
+    views.setOnClickPendingIntent(R.id.widget_temp, pendingMainIntent)
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
